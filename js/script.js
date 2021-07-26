f204bf817ca21968de1a8471f6d2e25eb5d74225
Alessandro2014 <alessandromerenda22@gmail.com>
2021-07-26 18:46:06 +0200

    filtri

--- js/script.js
+++ js/script.js
@@ -1,16 +1,39 @@
-// CICLO PER ITERARE NELL'ARRAY DI OGGETTI
-iconsTemplate = '';
-icons.forEach((item) => {
-    iconsTemplate += `
-    <div class="col-sm-offset-1 col-sm-2">
-        <div class="card txt-center-sm">
-            <i class="${item.family} ${item.prefix}${item.name} ${item.type}"></i>
-            <h5>${item.name.toUpperCase()}</h5>
+// CICLO PER ITERARE NELL'ARRAY DI OGGETTI 
+const printIcons = (icons, element) => {
+    iconsTemplate = '';
+    icons.forEach((item, index) => {
+        const offset = index % 5 === 0 ? 'col-md-offset-1' : '';
+        iconsTemplate += `
+        <div class="col-sm-4 col-md-2  ${offset}">
+            <div class="card txt-center-sm">
+                <i class="${item.family} ${item.prefix}${item.name} ${item.type}"></i>
+                <h5>${item.name.toUpperCase()}</h5>
+            </div>
         </div>
-    </div>
-    `;
+        `;
+    });
+    element.innerHTML = iconsTemplate;
+};
 
-});
-
+//STAMPA
 const sectionCard = document.querySelector('#icons .row');
-sectionCard.innerHTML = iconsTemplate;
+printIcons(icons, sectionCard);
+
+
+//FILTRO
+const selectField = document.getElementById('filter');
+
+selectField.addEventListener('change', () => {
+    const filterValue = selectField.value;
+    if(filterValue == 'all') {
+        printIcons(icons, sectionCard);
+        return;
+    }
+    const filterIcons = icons.filter((icon) => {
+        if(filterValue === icon.type) {
+            return true;
+        }
+        return false;
+    })
+    printIcons(filterIcons, sectionCard);
+});
